05b5cbad57b11a709dc3cc5b7ee44769195ee092
ryanfendi <102475046+ryanfendi@users.noreply.github.com>
2026-09-05 13:36:11 +0700

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'id.walpap.app',
+  appName: 'WALPAP',
+  webDir: 'www'
+};
+
+export default config;
