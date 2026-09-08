394f8a94459b5271f374edd41c9c415045c440a0
ryanfendi <102475046+ryanfendi@users.noreply.github.com>
2026-09-08 20:33:19 +0700

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'id.walpap.app',
-  appName: 'WALPAP',
-  webDir: 'www'
-};
-
-export default config;
